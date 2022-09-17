bd04f5cef8f5157f773bfddcfd79f439b25a9051
Tran Duc Duy Developer <109543062+tdd2003@users.noreply.github.com>
2022-09-17 23:07:14 +0700

    Update nasaki.py

--- nasaki.py
+++ nasaki.py
@@ -231,7 +231,7 @@ def menu():
 def main():
     menu()
     while(True):
-        cnc = input('''\x1b[38;2;0;212;14m╔══[tdd2006\x1b[38;2;0;186;45m@Na\x1b[38;2;0;150;88mx\x1b[38;2;0;113;133mSaKi\x1b[38;2;0;49;147m]
+        cnc = input('''\x1b[38;2;0;212;14m╔══[tdd2006\x1b[38;2;0;186;45m@Na\x1b[38;2;0;150;88m\x1b[38;2;0;113;133mSaKi\x1b[38;2;0;49;147m]
 \x1b[38;2;0;212;14m╚\x1b[38;2;0;186;45m═\x1b[38;2;0;150;88m═\x1b[38;2;0;113;133m═\x1b[38;2;0;83;168m═\x1b[38;2;0;49;147m➤ \x1b[38;2;239;239;239m''')
         if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
             layer7()
@@ -759,7 +759,58 @@ def login():
         sys.exit(1)
     elif username == user and password == passwd:
         print("⚡ Welcome to NaSaKi-V4")
-        time.sleep(3.5)
+        time.sleep(0.1)
+        ascii_vro()
+        main()
+
+login()
+def login():
+    clear()
+    user = "admin"
+    passwd = "tranducduy"
+    username = input("⚡ Username: ")
+    password = getpass.getpass(prompt='⚡ Password: ')
+    if username != user or password != passwd:
+        print("")
+        print("Không Làm Mà Đòi Có Ăn")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("⚡ Welcome to NaSaKi-V4")
+        time.sleep(0.1)
+        ascii_vro()
+        main()
+
+login()
+def login():
+    clear()
+    user = "admin"
+    passwd = "tranducduy"
+    username = input("⚡ Username: ")
+    password = getpass.getpass(prompt='⚡ Password: ')
+    if username != user or password != passwd:
+        print("")
+        print("Không Làm Mà Đòi Có Ăn")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("⚡ Welcome to NaSaKi-V4")
+        time.sleep(0.1)
+        ascii_vro()
+        main()
+
+login()
+def login():
+    clear()
+    user = "admin"
+    passwd = "tranducduy"
+    username = input("⚡ Username: ")
+    password = getpass.getpass(prompt='⚡ Password: ')
+    if username != user or password != passwd:
+        print("")
+        print("Không Làm Mà Đòi Có Ăn")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("⚡ Welcome to NaSaKi-V4")
+        time.sleep(0.1)
         ascii_vro()
         main()
 
