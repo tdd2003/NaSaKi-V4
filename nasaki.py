d866289ac4f1110f0c070364886508deaa37b437
Tran Duc Duy Developer <109543062+tdd2003@users.noreply.github.com>
2022-09-21 16:04:42 +0700

    Update nasaki.py

--- nasaki.py
+++ nasaki.py
@@ -221,7 +221,7 @@ def menu():
                 \x1b[38;2;0;212;14m║ \x1b[38;2;0;49;147m- - - - - - \x1b[38;2;239;239;239m DDoS Panel 2022\x1b[38;2;0;212;14m- - - - - - -\x1b[38;2;0;49;147m║
                 \x1b[38;2;0;212;14m╚═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╝
                     \x1b[38;2;0;212;14m╔═══════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════╗
-                    \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239mhttps://github.com/tdd2006/NaSaKi-V3 \x1b[38;2;0;49;147m║
+                    \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239mhttps://github.com/tdd2003/NaSaKi-V4 \x1b[38;2;0;49;147m║
                     \x1b[38;2;0;212;14m╚═══════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════╝
                 \x1b[38;2;0;212;14m╔═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╗
                 \x1b[38;2;0;212;14m║   \x1b[38;2;239;239;239m   Type help to see the all commands.      \x1b[38;2;0;49;147m║
@@ -737,13 +737,13 @@ RULES   ► RULES PANEL
 PORTS   ► SHOW ALL PORTS
 TOOLS   ► SHOW TOOLS
 CLEAR   ► CLEAR TERMINAL
-CHAT   ► CHAT GOBAL
+INFO   ► INFO ADMIN
             ''')
 
         else:
             try:
                 cmmnd = cnc.split()[0]
-                print("Command: [ " + cmmnd + " ] Not Found!")
+                print("Command: [ " + cmmnd + " ] Đéo Có Methods - Command Này!")
             except IndexError:
                 pass
 
@@ -752,95 +752,6 @@ def login():
     clear()
     user = "tdd"
     passwd = "tranducduy"
-    username = input("⚡ Username: ")
-    password = getpass.getpass(prompt='⚡ Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("Không Làm Mà Đòi Có Ăn")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("⚡ Welcome to NaSaKi-V4")
-        time.sleep(0.1)
-        ascii_vro()
-        main()
-
-login()
-def login():
-    clear()
-    user = "lah"
-    passwd = "lah"
-    username = input("⚡ Username: ")
-    password = getpass.getpass(prompt='⚡ Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("Không Làm Mà Đòi Có Ăn")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("⚡ Welcome to NaSaKi-V4")
-        time.sleep(0.1)
-        ascii_vro()
-        main()
-
-login()
-def login():
-    clear()
-    user = "test"
-    passwd = "123456"
-    username = input("⚡ Username: ")
-    password = getpass.getpass(prompt='⚡ Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("Không Làm Mà Đòi Có Ăn")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("⚡ Welcome to NaSaKi-V4")
-        time.sleep(0.1)
-        ascii_vro()
-        main()
-
-login()
-def login():
-    clear()
-    user = "tmh"
-    passwd = "tmh"
-    username = input("⚡ Username: ")
-    password = getpass.getpass(prompt='⚡ Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("Không Làm Mà Đòi Có Ăn")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("⚡ Welcome to NaSaKi-V4")
-        time.sleep(0.1)
-        ascii_vro()
-        main()
-
-login()
-
-login()
-def login():
-    clear()
-    user = "nht"
-    passwd = "nht"
-    username = input("⚡ Username: ")
-    password = getpass.getpass(prompt='⚡ Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("Không Làm Mà Đòi Có Ăn")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("⚡ Welcome to NaSaKi-V4")
-        time.sleep(0.1)
-        ascii_vro()
-        main()
-
-login()
-
-login()
-def login():
-    clear()
-    user = "nxt"
-    passwd = "nxt"
     username = input("⚡ Username: ")
     password = getpass.getpass(prompt='⚡ Password: ')
     if username != user or password != passwd:
